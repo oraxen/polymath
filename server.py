e8bc7a2a69bb8267475a862fccc227f4c31c583a
Thomas Marchand <thomas.marchand@tuta.io>
2020-02-09 17:23:20 +0100

    :truck: updates for atlas

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 from aiohttp import web
 
 # CONSTANTS
-SERVER_URL = "http://hestia.oraxen.com:8080" # The first dedicated vps for polymath
+SERVER_URL = "http://atlas.oraxen.com:8080" # The first dedicated vps for polymath
 PACKS_FOLDER = "./packs/"
 REGISTRY_FILE = './registry.json'
 REGISTRY = {}
